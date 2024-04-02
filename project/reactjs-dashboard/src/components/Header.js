316b57ed728f67192a3d9fcd78aadbc63c2154db
badrudeen20 <badruddinansary20@gmail.com>
2024-04-02 12:42:22 +0530

    react-dashboard-2

--- project/reactjs-dashboard/src/components/Header.js
+++ project/reactjs-dashboard/src/components/Header.js
@@ -1,55 +1,47 @@
 import React, {useState} from 'react'
 
 export default function Header({handleSidebar}) {
-//   const [toggle,setToggle] = useState(false)  
-//   function handleToggle(){
-//       setToggle(pre=>{
-//        const value = !pre
-//        handleSidebar(value)
-//        return value
-//       })
-      
-//   }
+
   return (
     <>
       <nav className='border'>
-            <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div class="relative flex h-16 items-center justify-between">
-                  <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+                  <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                   
-                  <button type="button" onClick={()=>handleSidebar(pre=>!pre)} class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-                  <span class="absolute -inset-0.5"></span>
-                  <span class="sr-only">Open main menu</span>
+                  <button type="button" onClick={()=>handleSidebar(pre=>!pre)} className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                  <span className="absolute -inset-0.5"></span>
+                  <span className="sr-only">Open main menu</span>
                   
-                  <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                  <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                   </svg>
                   
-                  <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                  <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                   </svg>
                   </button>
                   </div>
-                  <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                  <div class="flex flex-shrink-0 items-center">
+                  <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                  <div className="flex flex-shrink-0 items-center">
                   </div>
-                  <div class="hidden sm:ml-6 sm:block"></div>
+                  <div className="hidden sm:ml-6 sm:block"></div>
                   </div>
-                  <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                  <button type="button"  class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                  <span class="absolute -inset-1.5" ></span>
-                  <span class="sr-only">View notifications</span>
-                  <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                  <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                  <button type="button"  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                  <span className="absolute -inset-1.5" ></span>
+                  <span className="sr-only">View notifications</span>
+                  <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
                   </svg>
                   </button>
 
                   
-                  <div class="relative ml-3">
+                  <div className="relative ml-3">
                   <div>
-                        <button type="button" class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                        <span class="absolute -inset-1.5"></span>
-                        <span class="sr-only">Open user menu</span>
+                        <button type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                        <span className="absolute -inset-1.5"></span>
+                        <span className="sr-only">Open user menu</span>
                         
                         </button>
                   </div>
--- project/reactjs-dashboard/src/components/Statistic.js
+++ project/reactjs-dashboard/src/components/Statistic.js
@@ -11,32 +11,32 @@ export default function Statistic() {
   return (
     <>
     <div className='grid grid-cols-1 md:grid-cols-3 gap-3 m-2'>
-      <div class="rounded overflow-hidden shadow-lg ">
-            <div class="px-6 py-4">
-                  <div class="font-bold text-xl mb-2">
+      <div className="rounded overflow-hidden shadow-lg ">
+            <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2">
                   <PresentationChartBarIcon className="h-10 w-10" />
                   </div>
-                  <p class="text-gray-700 text-base">
+                  <p className="text-gray-700 text-base">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                   </p>
             </div>
       </div>
-      <div class="rounded overflow-hidden shadow-lg ">
-            <div class="px-6 py-4">
-                  <div class="font-bold text-xl mb-2">
+      <div className="rounded overflow-hidden shadow-lg ">
+            <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2">
                   <ShoppingBagIcon className="h-10 w-10" />
                   </div>
-                  <p class="text-gray-700 text-base">
+                  <p className="text-gray-700 text-base">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                   </p>
             </div>
       </div>
-      <div class="rounded overflow-hidden shadow-lg ">
-            <div class="px-6 py-4">
-                  <div class="font-bold text-xl mb-2">
+      <div className="rounded overflow-hidden shadow-lg ">
+            <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2">
                   <InboxIcon className="h-10 w-10" />
                   </div>
-                  <p class="text-gray-700 text-base">
+                  <p className="text-gray-700 text-base">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                   </p>
             </div>
